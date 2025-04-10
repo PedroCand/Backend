3e91fd07ca80f12213b6bcaebaaf4d74c82fecd7
PedroCand <candidomachadopedro@gmail.com>
2025-04-10 11:30:44 -0400

    atividades 02

---
+++ operadores.js
@@ -0,0 +1,24 @@
+//1
+let vlr1 = 10;
+let vlr2 = 12;
+console.log(vlr1 + vlr2);
+console.log(vlr1 - vlr2);
+console.log(vlr1 * vlr2);
+console.log(vlr1 / vlr2);
+
+//2
+let idade1 = 24;
+let idade2 = 16;
+let comp = idade1 > idade2 ? 'o primeiro é mais velho' : 'o segundo é mais velho';
+console.log(comp);
+
+//3
+let num = 17;
+let compa = num > 18 ? 'maior de idade' : 'menor de idade'
+console.log(compa);
+
+//4
+let nume = 18;
+let resto = nume % 2;
+let resul = resto == 0 ? 'o número é par' : 'O número é impar';
+console.log(resul);
